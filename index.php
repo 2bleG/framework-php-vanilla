63cedc5c3c7b1d515a6f7e03f800c9b2fc813ede
2bleG <2bleg.akagucci@gmail.com>
2023-10-06 15:55:02 +0200

    index

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-
 require_once './configs/bootstrap.php';
+
 ob_start();
 if(isset($_GET["page"])){
     fromInc($_GET['page']);
 }
 
-$connexion->query(queryBuilder('r','contact'))
+$connection->query(queryBuilder('r','contact'));
 $pageContent = [
     "html" => ob_get_clean(),
     "data" => [
@@ -15,4 +15,3 @@ $pageContent = [
 ];
 
 include "./templates/layouts/". $_GET["layout"] .".layout.php";
-
