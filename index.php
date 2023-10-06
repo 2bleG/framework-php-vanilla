b544261790570d787ec294c69946773d8c44e3c1
2bleG <2bleg.akagucci@gmail.com>
2023-10-06 15:32:04 +0200

    up

--- index.php
+++ index.php
@@ -6,8 +6,12 @@ if(isset($_GET["page"])){
     fromInc($_GET['page']);
 }
 
+$connexion->query(queryBuilder('r','contact'))
 $pageContent = [
-    "html" => ob_get_clean()
+    "html" => ob_get_clean(),
+    "data" => [
+        'contacts' => []
+    ]
 ];
 
 include "./templates/layouts/". $_GET["layout"] .".layout.php";
--- templates/layouts/html.layout.php
+++ templates/layouts/html.layout.php
@@ -2,4 +2,17 @@
 include_once './templates/includes/html_header.inc.php';
 fromInc("menu");
 echo $pageContent['html'];
+?>
+
+<ul>
+<?php
+
+    foreach ($pageContent["data"]['contact'] as $key=> $value){
+        
+    };
+
+?>
+</ul>
+
+<?php
 include_once './templates/includes/html_footer.inc.php';
